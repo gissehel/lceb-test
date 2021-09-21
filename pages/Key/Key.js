edf2b069a40c3f2ad7ff21ebfcb133eb3d866546
gissehel <public-github@gissehel.org>
2021-09-21 19:19:35 +0000

    FEATURE : Add selected / disabled status to Keys

--- pages/Key/Key.js
+++ pages/Key/Key.js
@@ -2,9 +2,25 @@ import classnames from 'classnames'
 
 import styles from './Key.module.css'
 
-const Key = ({text, onClick, disabled}) => {
-    return <div className={classnames(styles.Key, {[styles.disabled]:disabled, [styles.enabled]:!disabled})} onClick={onClick}>
-        <div className={styles.inner}>{text}</div>
+const Key = ({text, subtext, onClick, disabled, selected}) => {
+    const className = classnames(
+        styles.Key, 
+        {
+            [styles.disabled]:disabled, 
+            [styles.enabled]:!disabled,
+            [styles.selected]:selected,
+        }
+    );
+    return <div className={className} onClick={onClick}>
+        <div className={styles.inner}>
+            {text}
+            {
+                subtext ?
+                <div className={styles.subtext}>{subtext}</div> :
+                null
+            }
+            
+        </div>
     </div>
 }
 
